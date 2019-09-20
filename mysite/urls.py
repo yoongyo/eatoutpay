0b69ced8a0cdae94423965dbcd1807d1e8f4249b
yoongyo <jyg0172@naver.com>
2019-09-21 00:35:51 +0900

    Ad

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,8 +2,6 @@ from django.conf.urls import re_path, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-
-from graphene_file_upload.django import FileUploadGraphQLView
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
 
@@ -12,7 +10,7 @@ urlpatterns = [
     re_path(r'^', include(('restaurant.urls', 'restaurant'), namespace='restaurant')),
     re_path('accounts/', include(('accounts.urls', 'accounts'), namespace='accounts')),
     re_path(r'^admin/', admin.site.urls),
-    re_path(r'^graphql/', csrf_exempt(FileUploadGraphQLView.as_view(graphiql=True))),
+    re_path(r'^graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
 
 
