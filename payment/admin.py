a0834d923c7f4446d044d11d385798df425faacb
yoongyo <jyg0172@naver.com>
2019-09-20 16:30:57 +0900

    AD

--- payment/admin.py
+++ payment/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Basket, PayMethod
 
-# Register your models here.
+
+class BasketAdmin(admin.ModelAdmin):
+    list_display = ['user']
+
+
+class PayMethodAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+admin.site.register(Basket, BasketAdmin)
+admin.site.register(PayMethod, PayMethodAdmin)
+
