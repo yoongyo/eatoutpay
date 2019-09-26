6279a1adb5c54f376e6e33e277ecf65cd15b502d
yoongyo <jyg0172@naver.com>
2019-09-26 22:57:52 +0900

    DA

--- payment/models.py
+++ payment/models.py
@@ -14,7 +14,7 @@ class Basket(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     sumPrice = models.CharField(max_length=50)
-    menus = models.CharField(max_length=200)
+    menus = models.TextField()
     payment = models.ForeignKey(PayMethod, on_delete=models.CASCADE)
     dateTime = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     date = models.DateField(auto_now_add=True, null=True, blank=True)
